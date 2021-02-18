32161d0306798b9604d992ad1606058c429aa08c
Benjamin <carlosbpac@gmail.com>
2021-02-17 21:37:35 -0600

    Generacion de figuras y graficado de las mismas

--- APP_practica_1/app/src/main/java/com/mycompany/app_practica_1/UI/lienzo.java
+++ APP_practica_1/app/src/main/java/com/mycompany/app_practica_1/UI/lienzo.java
@@ -3,7 +3,10 @@ package com.mycompany.app_practica_1.UI;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.Build;
 import android.view.View;
+
+import androidx.annotation.RequiresApi;
 
 import com.mycompany.app_practica_1.Instructions.Circulo;
 import com.mycompany.app_practica_1.Instructions.Cuadrado;
@@ -15,7 +18,7 @@ import java.util.List;
 
 public class lienzo extends View {
     private List<Object> sentencias;
-
+    private static final int ANCHO = 15;
     public lienzo(Context context,List<Object> sentencias){
         super(context);
         this.sentencias=sentencias;
@@ -36,24 +39,65 @@ public class lienzo extends View {
 
     }
 
-    public void dibujarFiguras(List<Object> sentencias,Canvas canvas){
+    public void dibujarFiguras(List<Object> sentencias, Canvas canvas){
         for(Object obj: sentencias){
             if(obj instanceof Circulo){
                 Circulo tmp = (Circulo)obj;
+                /*
                 canvas.drawCircle(tmp.getPosx(),tmp.getPosy(),tmp.getRadio(),color(tmp.getColor()));
-                canvas.drawCircle(tmp.getPosx(),tmp.getPosy(),(tmp.getRadio()-10),color("default"));
+                canvas.drawCircle(tmp.getPosx(),tmp.getPosy(),(tmp.getRadio()-ANCHO),color("default"));
+                */
+
+                Paint tmpPincel = color(tmp.getColor());
+                tmpPincel.setStrokeWidth(ANCHO);
+                double j =0;
+                while (j<361){
+                    float x,y,xt,yt;
+                    xt = (float) (tmp.getRadio()*(Math.cos((j*Math.PI/180))));
+                    yt = (float) (tmp.getRadio()*(Math.sin(j*Math.PI/180)));
+
+                    x=tmp.getPosx()+xt;
+                    y=tmp.getPosy()+yt;
+                    canvas.drawPoint(x,y,tmpPincel);
+                    j=j+0.1;
+                }
+                /*
+                for (int i =0;i<=360;i++){
+                    float x,y,xt,yt;
+                    xt = (float) (tmp.getRadio()*(Math.cos(i)));
+                    yt = (float) (tmp.getRadio()*(Math.sin(i)));
+
+                    x=tmp.getPosx()+xt;
+                    y=tmp.getPosy()+yt;
+                    canvas.drawPoint(x,y,tmpPincel);
+                }
+                */
+
+
             }
             if(obj instanceof Cuadrado){
+                Cuadrado tmp = (Cuadrado)obj;
+                this.drawCuadrilatero(tmp.getPosx(),tmp.getPosy(),tmp.getTamLado(),tmp.getTamLado(),tmp.getColor(),canvas);
+
+                /*
+                canvas.drawRect(tmp.getPosx(),tmp.getPosy(),tmp.getPosx()+tmp.getTamLado(),tmp.getPosy()+tmp.getTamLado(),color(tmp.getColor()));
+                canvas.drawRect(tmp.getPosx()+ANCHO,tmp.getPosy()+ANCHO,tmp.getPosx()+tmp.getTamLado()-ANCHO,tmp.getPosy()+tmp.getTamLado()-ANCHO,color("default"));
+                */
 
             }
             if(obj instanceof Linea){
-
+                Linea tmp =(Linea)obj;
+                Paint tmpPincel = color(tmp.getColor());
+                tmpPincel.setStrokeWidth(ANCHO);
+                canvas.drawLine(tmp.getPosx(),tmp.getPosy(),tmp.getPosx2(),tmp.getPosy2(),tmpPincel);
             }
             if(obj instanceof Poligono){
 
+
             }
             if(obj instanceof Rectangulo){
-
+                Rectangulo tmp = (Rectangulo)obj;
+                this.drawCuadrilatero(tmp.getPosx(),tmp.getPosy(),tmp.getAlto(),tmp.getAncho(),tmp.getColor(),canvas);
             }
         }
     }
@@ -89,6 +133,30 @@ public class lienzo extends View {
                 pincel.setARGB(255,255,255,255);
                 return pincel;
         }
+
+    }
+
+    public void drawCuadrilatero(int px,int py,int alto,int ancho,String color,Canvas canvas){
+        if(px==0){
+            px=ANCHO/2;
+        }
+        if(py==0){
+            py=ANCHO/2;
+        }
+        Paint tmpPincel =  color(color);
+        tmpPincel.setStrokeWidth(ANCHO);
+        //ARRIBA
+        canvas.drawLine(px-(ANCHO/2),py,px+ancho+(ANCHO/2),py,tmpPincel);
+        //LADO DERECHO
+        canvas.drawLine(px,py,px,py+alto,tmpPincel);
+        //ABAJO
+        canvas.drawLine(px-(ANCHO/2),py+alto,px+ancho+(ANCHO/2),py+alto,tmpPincel);
+        //LADO IZQUIERDO
+        canvas.drawLine(px+ancho,py,px+ancho,py+alto,tmpPincel);
+    }
+    public void drawPoligono(Canvas canvas,int numLados){
+        double angulos[]=new double[numLados+1];
+
 
     }
 
--- PruebasPractica1Compiladores/src/main/java/com/mycompany/pruebaspractica1compiladores/main.java
+++ PruebasPractica1Compiladores/src/main/java/com/mycompany/pruebaspractica1compiladores/main.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.pruebaspractica1compiladores;
 
+import com.mycompany.pruebaspractica1compiladores.Instructions.Point;
 import com.mycompany.pruebaspractica1compiladores.Instructions.Settings;
 import com.mycompany.pruebaspractica1compiladores.Lexer.appLexer;
 import com.mycompany.pruebaspractica1compiladores.Parcer.Sincronizar;
@@ -21,6 +22,7 @@ import java.util.logging.Logger;
 public class main {
 
     public static void main(String[] args) {
+        /*
         String cadenas
                 = "graficar circulo ( 1 + 12, 25, 15/3, rojo)\n"
                 + "graficar linea ( 12 * 3 + 2, 15, 4 / 4, 50 * 1, amarillo)\n"
@@ -32,6 +34,7 @@ public class main {
         /*String cadenas = 
                 "graficar circulo ( (3+2)*2, 2, 3, rojo)"
                 ;*/
+        /*
         cadenas="";
         appLexer lex = new appLexer(new StringReader(cadenas));
         parser appParser = new parser(lex);
@@ -64,6 +67,46 @@ public class main {
             System.out.println("Error irrecuperable, " + ex.getMessage());
             ex.printStackTrace();
         }
-
+        */
+        double a=1.7,b=2;
+        int lados = 3;
+        
+        double angulos[]= new double[lados+1];
+        Point puntos[] = new Point[lados+1];
+        
+        for (int i = 0; i <= lados; i++) {
+            angulos[i]=(i*(360/lados));
+        }
+        double tx,ty,r;
+        int cont =0;
+        for(double an:angulos){
+            r = radio(an, a, b);
+            tx = r*Math.cos(an*Math.PI/180);
+            ty = r*Math.sin(an*Math.PI/180);
+            System.out.println("Angulo: "+an+" radio= "+r+" ,x= "+tx+" ,y= "+ty);
+            puntos[cont]=new Point(tx, ty);
+            cont++;
+        }
+        for(Point p:puntos){
+            System.out.println(p.toString());
+        }
+        
+        
     }
+    private static double radio(double angulo,double a,double b){
+        double tmp1,tmp2,tmp3,cos,sin;
+        
+        cos = Math.cos(angulo*Math.PI/180);
+        
+        sin = Math.sin(angulo*Math.PI/180);
+       
+        
+        tmp1 = (Math.pow(cos, 2))/(Math.pow(a, 2));
+        tmp2 = (Math.pow(sin, 2))/(Math.pow(b, 2));
+        
+        tmp3 = Math.sqrt(1/(tmp1+tmp2));
+        
+        return tmp3;
+    }
+    
 }
